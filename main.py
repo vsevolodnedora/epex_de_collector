65241a104bf1f817a9cdd92c8d66c66ffabd071d
Vsevolod <fizik.vlk@gmail.com>
2024-10-28 14:13:06 +0100

    upd

--- main.py
+++ main.py
@@ -14,9 +14,11 @@ from datetime import datetime,timedelta
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
+import warnings
 import os
+warnings.simplefilter(action='ignore', category=FutureWarning)
 
-def get_time_axis_hour(date_str:str,start_hour:int) -> pd.Series:
+def get_time_axis_hour(date_str:str,start_hour:int) -> pd.Series :
     index_hours = [
         "00 - 01", "01 - 02", "02 - 03", "03 - 04", "04 - 05", "05 - 06",
         "06 - 07", "07 - 08", "08 - 09", "09 - 10", "10 - 11", "11 - 12",
